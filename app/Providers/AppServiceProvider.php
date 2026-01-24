39c01182db0f0b356bf6d3f3e9cfe65e16974dbf
CiborgLuiz <126992162+CiborgLuiz@users.noreply.github.com>
2026-01-24 01:25:46 -0300

    Revertendo problema

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,8 +2,6 @@
 
 namespace App\Providers;
 
-use Illuminate\Support\Facades\Schema;
-use Illuminate\Support\Facades\Artisan;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
@@ -21,30 +19,6 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-    // Evita erro de key length no PostgreSQL
-        Schema::defaultStringLength(191);
-
-    // Rodar migrations automaticamente em produção
-        if (app()->environment('production')) {
-            try {
-                Artisan::call('migrate', [
-                    '--force' => true
-                ]);
-            } catch (\Throwable $e) {
-            // silencioso para não derrubar o app
-            }
-        }
-        if (app()->environment('production')) {
-            if (!file_exists(public_path('storage'))) {
-                try {
-                    Artisan::call('storage:link');
-                } catch (\Throwable $e) {}
-            }
-        }
-        if (app()->environment('production')) {
-            Artisan::call('config:cache');
-            Artisan::call('route:cache');
-            Artisan::call('view:cache');
-        }
+        //
     }
 }
